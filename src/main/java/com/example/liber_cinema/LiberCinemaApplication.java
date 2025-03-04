cca9bd0207abb03a809940f15c1d9adf24964577
Kacper Samolej <kacper.sa7@gmail.com>
2025-03-04 14:57:52 +0100

    Initial commit

---
+++ src/main/java/com/example/liber_cinema/LiberCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.liber_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberCinemaApplication.class, args);
+	}
+
+}
