ac8bb0ab3bad6efcbef6f22ef25e96052ac9588d
Kacper Samolej <kacper.sa7@gmail.com>
2025-03-16 23:05:13 +0100

    feat: add endpoints for fetching all movies and books or by ID, update indexing to start from 0, add POST endpoint for adding books or movies

--- src/main/java/com/example/liber_cinema/BookController.java
+++ src/main/java/com/example/liber_cinema/BookController.java
@@ -1,8 +1,7 @@
 package com.example.liber_cinema;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +13,36 @@ public class BookController {
     private static final Map<Integer, String> books = new HashMap<>();
 
     static {
-        books.put(1, "Pan Tadeusz");
-        books.put(2, "Harry Potter");
-        books.put(3, "Atomic Habits");
-        books.put(4, "Krzyżacy");
-        books.put(5, "Costam");
+        books.put(0, "Pan Tadeusz");
+        books.put(1, "Harry Potter");
+        books.put(2, "Atomic Habits");
+        books.put(3, "Krzyżacy");
+        books.put(4, "Costam");
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getBooks(){
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<Object> getBookById(@PathVariable int bookId){
+        String book = books.get(bookId);
+        if(book == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(books.get(bookId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> addNewBook(@RequestBody String book){
+        if(book == null || books.containsKey(book)) {
+            return ResponseEntity.badRequest().body("Book title cannot be empty");
+        }
+        if (books.containsValue(book)) {
+            return ResponseEntity.badRequest().body("Book already exists");
+        }
+        books.put(books.size(), book);
+        return ResponseEntity.status(201).body("Book added successfully");
     }
 }
--- src/main/java/com/example/liber_cinema/MovieController.java
+++ src/main/java/com/example/liber_cinema/MovieController.java
@@ -2,9 +2,7 @@ package com.example.liber_cinema;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,10 +14,35 @@ public class MovieController {
 
     private static final Map<Integer, String> movies = new HashMap<>();
     static {
-        movies.put(1, "Interstellar");
-        movies.put(2, "Harry Potter");
-        movies.put(3, "Star Wars");
-        movies.put(4, "The Godfather");
-        movies.put(5, "John Wick");
+        movies.put(0, "Interstellar");
+        movies.put(1, "Harry Potter");
+        movies.put(2, "Star Wars");
+        movies.put(3, "The Godfather");
+        movies.put(4, "John Wick");
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getMovies(){
+        return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/{movieId}")
+    public ResponseEntity<Object> getMovieById(@PathVariable int movieId){
+        String movie = movies.get(movieId);
+        if(movie == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movies.get(movieId));
+    }
+    @PostMapping
+    public ResponseEntity<Object> addNewMovie(@RequestBody String movie){
+        if(movie == null || movies.containsKey(movie)) {
+            return ResponseEntity.badRequest().body("Movie title cannot be empty");
+        }
+        if (movies.containsValue(movie)) {
+            return ResponseEntity.badRequest().body("Movie already exists");
+        }
+        movies.put(movies.size(), movie);
+        return ResponseEntity.status(201).body("Movie added successfully");
     }
 }
