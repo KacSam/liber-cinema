cb9496f6092455051db2e68cecc43773989fe1db
Kacper Samolej <kacper.sa7@gmail.com>
2025-03-31 14:57:24 +0200

    feat: add models of Movie and Book

--- src/main/java/com/example/liber_cinema/models/Book.java
+++ src/main/java/com/example/liber_cinema/models/Book.java
@@ -1,34 +1,38 @@
 package com.example.liber_cinema.models;
 
 
+
 import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "movies")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class Movie {
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String title;
 
-    private String description;
+    private String author;
     private String genre;
-    private String director;
-    private String releaseDate;
-    private String duration;
-    private double imdbRating;
+    private String publicationDate;
+    private String description;
+    private String externalRating;
 
     @OneToMany
     private Set<Rating> ratings;
 
     @OneToMany
     private Set<UserList> userLists;
-
 }
