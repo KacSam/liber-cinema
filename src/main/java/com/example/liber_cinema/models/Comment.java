4cc403069eb986b9a7783d6934fc02c5a84a94f0
Kacper Samolej <kacper.sa7@gmail.com>
2025-06-01 21:59:39 +0200

    feat(api): add Comment model

---
+++ src/main/java/com/example/liber_cinema/models/Comment.java
@@ -0,0 +1,39 @@
+package com.example.liber_cinema.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    
+    @ManyToOne
+    @JoinColumn(name = "activity_id", nullable = false)
+    private UserList activity;
+    
+    @Column(nullable = false, length = 500)
+    private String content;
+    
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
