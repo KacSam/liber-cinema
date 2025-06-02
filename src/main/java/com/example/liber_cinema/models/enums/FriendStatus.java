0dc2f8d9fea8966a7b4f2d15cd7614db78e87dd6
Kacper Samolej <kacper.sa7@gmail.com>
2025-06-02 13:25:07 +0200

    feat(api): add FriendStatus nad NotificationType enums

---
+++ src/main/java/com/example/liber_cinema/models/enums/FriendStatus.java
@@ -0,0 +1,6 @@
+package com.example.liber_cinema.models.enums;
+
+public enum FriendStatus {
+    PENDING,    // Oczekujące zaproszenie
+    ACCEPTED    // Zaakceptowane zaproszenie
+}
---
+++ src/main/java/com/example/liber_cinema/models/enums/NotificationType.java
@@ -0,0 +1,8 @@
+package com.example.liber_cinema.models.enums;
+
+public enum NotificationType {
+    FRIEND_REQUEST,
+    FRIEND_ACCEPT,
+    COMMENT,
+    SYSTEM
+}
